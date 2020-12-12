c6fd2abe6a0206cfcb4b24d5439fad44e0ffe7d8
andres <afmartinez099@misena.edu.co>
2020-12-11 22:56:07 -0500

    commit ejercicio fin de trimestre

---
+++ JS/EJERCICIO.js
@@ -0,0 +1,11 @@
+var clave = document.getElementById ("password").value;
+var usuario = document.getElementById ("email").value;
+
+
+function validador () {
+
+    if ((usuario.value == "afmartinez099@misena.edu.co") && (clave.value == "1234")){
+    window.open="https://www.facebook.com/";}
+
+    else {alert("Datos incorrectos")}}
+
